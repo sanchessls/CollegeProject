723fcc9fbd53a8b063d85508e4ed82e0b56dc94c
AndreSanches <sanchessls@gmail.com>
2020-10-16 16:13:19 +0100

    Deleting base controller .cs

--- ScrumPokerAPI/Startup.cs
+++ ScrumPokerAPI/Startup.cs
@@ -22,9 +22,16 @@ namespace ScrumPokerAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Here we are adding to the Entity Framework our Db Context with our connection string 
             services.AddDbContext<ApplicationContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("ScrumPokerConnection")));
             
+            //Add all of the controllers 
             services.AddControllers();
+
+            //Adding the repository to an interface as Scooped (One instance per request)
+            //this is used in our Dependence injection
+            //Everytime that the system ask for the first param, the system will serve an instance of the second one
+            //This allow us to change the repository without haveing to change the original one.
 
             services.AddScoped<IRepositoryTableOne, RepositoryTableOneImp>();
             services.AddScoped<IRepositoryTableTwo, RepositoryTableTwoImp>();
