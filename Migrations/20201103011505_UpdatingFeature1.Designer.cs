149e50c90a0fb84d12711ca48416aa09297bdc9e
AndreSanches <sanchessls@gmail.com>
2020-11-03 01:17:55 +0000

    CI migration test

--- Migrations/20201103011505_UpdatingFeature1.Designer.cs
+++ Migrations/20201103011505_UpdatingFeature1.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ScrumPokerPlanning.Context;
 
 namespace ScrumPokerPlanning.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20201103011505_UpdatingFeature1")]
+    partial class UpdatingFeature1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
